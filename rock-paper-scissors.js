f2ac147e7ee3842e4d278c3392f1f38f77464ea5
Tientt27 <tientatree.949@gmail.com>
2024-11-16 10:53:30 +0100

    Create a new branch

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,78 +1,29 @@
-console.log("hello")
 
-//Score variables declaration (Global scope)
-let humanScore = 0;
-let computerScore = 0;
-
-//It could be possible to add an arrow function instead of this
-
-let computerChoice = function getComputerChoice(){
-    let randomToThree = Math.floor(Math.random() * 3);
-    if (randomToThree === 0){ 
-         //console.log("Rock");
-         return ("Rock")
-        
-    } else if (randomToThree === 1){
-        //console.log("Paper"); 
-        return ("Paper")
-    } else {
-        //console.log("Scissors");
-        return ("Scissors")
+//Create computer choice
+function getComputerChoice(){
+    let mathComputer = Math.floor(Math.random() * 3 + 1);
+    if (mathComputer === 1){
+        return "rock"
+    } else if (mathComputer === 2){
+        return "paper"
+    } else if (mathComputer === 3){
+        return "scissors"
     }
+   
 }
 
+//Create the human response
 
-let humanChoice = function getHumanChoice(){
-    
-    prompt("Rock, Paper or Scissors?")
-    //console.log(humanChoice);
-
-    if(humanChoice.toLowerCase() === "rock"){
-        //console.log("Rock");
-         return ("Rock")
-    } else if (humanChoice.toLowerCase() === "paper"){
-        //console.log("Paper");
-         return ("Paper")
-        
-    } else if (humanChoice.toLowerCase() === "scissors") {
-        //console.log("Scissors")
-         return ("Scissors")
-    } else {
-        console.log("no")
-    }
-}
+ function getHumanChoice(){
+     let promptHuman = prompt("Rock, paper or scissors?");
+     if (promptHuman.toLowerCase === "rock"){
+        return "rock";
+     } else if (promptHuman.toLowerCase === "paper"){
+        return "paper";
+     } else if (promptHuman.toLowerCase === "scissors"){
+         return "scissors";
+     }
+     
 
 
-function playRound(){
-
-
-    //Draw statement
-    if (computerChoice === humanChoice){
-        console.log("Draw")
-        humanScore + 1
-
-        //Other posibilities
-        //When losing
-
-    } else if (humanChoice === "Scissors"  && computerChoice === "Rock"){
-        console.log("You loose! Rock beats Scissors")
-
-    } else if (humanChoice === "Paper"  && computerChoice === "Scissors"){
-        console.log("You loose! Scissors beat Paper")
-
-    } else if (humanChoice === "Rock"  && computerChoice === "Paper"){
-        console.log("You loose! Paper beats Rock")
-
-        //When winning
-
-    } else if (humanChoice === "Scissors"  && computerChoice === "Paper"){
-        console.log("You win! Scissors beat Paper")
-
-    } else if (humanChoice === "Paper"  && computerChoice === "Rock"){
-        console.log("You win! Paper beats Rock")
-        
-    } else if (humanChoice === "Rock"  && computerChoice === "Scissors"){
-        console.log("You win! Rock beats Scissors")
-    }
-    
-}
+ }
